a2c23c0b564be12c74074b3fadb42d5077886a1c
Константин Дьяченко <konard@me.com>
2019-07-29 21:08:04 +0300

    Initial code commit, forked from: https://github.com/Konard/LinksPlatform

---
+++ Constants/ILinksDecisionConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Platform.Data.Constants
+{
+    public interface ILinksDecisionConstants<TDecision>
+    {
+        /// <summary>Возвращает булевское значение, обозначающее продолжение прохода по связям.</summary>
+        /// <remarks>Используется в функции обработчике, который передаётся в функцию Each.</remarks>
+        TDecision Continue { get; }
+
+        /// <summary>Возвращает булевское значение, обозначающее остановку прохода по связям.</summary>
+        /// <remarks>Используется в функции обработчике, который передаётся в функцию Each.</remarks>
+        TDecision Break { get; }
+    }
+}
---
+++ Exceptions/LinkWithSameValueAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Platform.Data.Exceptions
+{
+    public class LinkWithSameValueAlreadyExistsException : Exception
+    {
+        public static readonly string DefaultMessage = "Связь с таким же значением уже существует.";
+        public LinkWithSameValueAlreadyExistsException(string message) : base(message) { }
+        public LinkWithSameValueAlreadyExistsException() : base(DefaultMessage) { }
+    }
+}
