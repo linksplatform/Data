f65b798b23b5ff2a58ec44a8524b26a9ccf2ae32
FreePhoenix <freephoenix888@gmail.com>
2021-11-20 23:26:28 +0600

    Add ILinksTests

---
+++ cpp/Platform.Data.Tests/ILinksTests.cpp
@@ -0,0 +1,31 @@
+namespace Platform::Data::Tests
+{
+    template<std::integral TLinkAddress>
+    struct Links : public ILinks<Links<TLinkAddress>, TLinkAddress, LinksConstants<TLinkAddress>>
+    {
+        TLinkAddress Count(Interfaces::IArray auto&& restriction) const { return 0; }
+    
+        TLinkAddress Each(auto&& handler, Interfaces::IArray auto& restrictions)
+        { return 0; }
+    
+        TLinkAddress Each(auto&& handler, const Interfaces::IArray auto& restrictions) const
+        { return 0; }
+    
+        TLinkAddress Create(Interfaces::IArray auto&& restriction) { return 0; }
+    
+        TLinkAddress Update(Interfaces::IArray auto&& substitution, std::convertible_to<TLinkAddress> auto... restrictions) { return 0; }
+    
+        void Delete(Interfaces::IArray auto&& restriction) {  }
+    };
+    TEST(ILinksDeriverTest, ConstructorAndMethodsTest)
+    {
+        using TLink = uint64_t;
+        Links<TLink> links{};
+        int restriction[]{1,2,3};
+        links.Create(restriction);
+        links.Update(restriction, 1);
+        links.Count(restriction);
+        links.Each([](TLink restriction_a){ return static_cast<TLink>(1); }, restriction);
+        links.Delete(restriction);
+    }
+}
