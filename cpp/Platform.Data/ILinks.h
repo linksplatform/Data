a9f7e55202d7de76e561c4901800d4cbf93f20cb
FreePhoenix888 <freephoenix888@gmail.com>
2021-11-25 22:47:14 +0600

    Update Update() parameters
    Remove Interfaces::Array for substitution parameter

--- cpp/Platform.Data/ILinks.h
+++ cpp/Platform.Data/ILinks.h
@@ -21,7 +21,7 @@
 
         TLinkAddress Create(Interfaces::IArray auto&& restriction) { return self().Create(restriction); }
 
-        TLinkAddress Update(Interfaces::IArray auto&& substitution, std::convertible_to<TLinkAddress> auto... restrictions) { return self().Update(substitution, restrictions...); }
+        TLinkAddress Update(auto&& substitution, std::convertible_to<TLinkAddress> auto... restrictions) { return self().Update(substitution, restrictions...); }
 
         void Delete(Interfaces::IArray auto&& restriction) { self().Delete(restriction); }
 
