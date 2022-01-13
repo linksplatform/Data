8fd1aeddad37aec6487550ee375bcf98d3c39f4d
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-13 14:16:26 +0600

    Import `Platform.Delegates`

--- csharp/Platform.Data/Universal/IUniLinks.cs
+++ csharp/Platform.Data/Universal/IUniLinks.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Platform.Delegates;
 
 // ReSharper disable TypeParameterCanBeVariant
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
