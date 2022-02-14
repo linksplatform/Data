421ef67cb9b815b5a3d2524ee3dbb9cb2e2cba38
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-14 14:10:25 +0600

    Add .net6 support

---
+++ cpp/cmake-build-debug/CMakeFiles/Platform.Data.Tests.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/freephoenix888/Documents/Programming/LinksPlatform/Data/cpp/Platform.Data.Tests/AllTests.cpp" "/home/freephoenix888/Documents/Programming/LinksPlatform/Data/cpp/cmake-build-debug/CMakeFiles/Platform.Data.Tests.dir/Platform.Data.Tests/AllTests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Platform.Data"
+  "/home/freephoenix888/.conan/data/platform.interfaces/0.1.4/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/freephoenix888/.conan/data/platform.ranges/0.1.3/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/freephoenix888/.conan/data/platform.converters/0.1.0/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/freephoenix888/.conan/data/platform.hashing/0.2.0/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/freephoenix888/.conan/data/platform.exceptions/0.2.0/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/freephoenix888/.conan/data/platform.delegates/0.1.3/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/freephoenix888/.conan/data/platform.setters/0.0.1/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/freephoenix888/.conan/data/gtest/cci.20210126/_/_/package/e019a06362b932ca5d1b082b6c112aa150c88de4/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
