5e1119f8f2eff2ee3474da460a9948bdddb8d61e
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-23 12:04:25 +0600

    Remove default TLink value

--- cpp/Platform.Data/LinksOptions.h
+++ cpp/Platform.Data/LinksOptions.h
@@ -1,6 +1,6 @@
 namespace Platform::Data
 {
-    template<typename TLink = Link, typename TWriteHandler = std::function<typename TLink::value_type(TLink, TLink)>, typename TReadHandler = std::function<typename TLink::value_type(TLink)>, LinksConstants<typename TLink::value_type> VConstants = LinksConstants<typename TLink::value_type>{}>
+    template<typename TLink, typename TWriteHandler = std::function<typename TLink::value_type(TLink, TLink)>, typename TReadHandler = std::function<typename TLink::value_type(TLink)>, LinksConstants<typename TLink::value_type> VConstants = LinksConstants<typename TLink::value_type>{true}>
     struct LinksOptions
     {
         using LinkType = TLink;
