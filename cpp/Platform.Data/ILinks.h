51d429d00492c7bf7a00fa1db133fae6cdecca06
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-23 12:05:26 +0600

    Rename `LinkTypesOptions` to `TLinksOptions`, `Options` to `LinksOptionsType`

--- cpp/Platform.Data/ILinks.h
+++ cpp/Platform.Data/ILinks.h
@@ -1,35 +1,25 @@
 ﻿namespace Platform::Data
 {
     using namespace Platform::Interfaces;
-    template<typename LinkTypesOptions>
+    template<typename TLinksOptions>
     struct ILinks
     {
     public:
-        using Options = LinkTypesOptions;
-        using LinkType = typename Options::LinkType;
+        using LinksOptionsType = TLinksOptions;
+        using LinkType = typename LinksOptionsType::LinkType;
         using LinkAddressType = typename LinkType::value_type;
-        using WriteHandlerType = typename Options::TWriteHandler;
-        using ReadHandlerType = typename Options::TReadHandler;
-        static constexpr LinksConstants<LinkAddressType> Constants = Options::Constants;
+        using WriteHandlerType = typename LinksOptionsType::TWriteHandler;
+        using ReadHandlerType = typename LinksOptionsType::TReadHandler;
+        static constexpr LinksConstants<LinkAddressType> Constants = LinksOptionsType::Constants;
 
         virtual LinkAddressType Count(const LinkType& restriction) = 0;
 
         virtual LinkAddressType Each(const LinkType& restrictions, const ReadHandlerType& handler) = 0;
 
-        virtual LinkAddressType Each(const LinkType&& restrictions, const ReadHandlerType& handler) = 0;
-
         virtual LinkAddressType Create(const LinkType& restrictions, const WriteHandlerType& handler) = 0;
-
-        virtual LinkAddressType Create(LinkType&& restrictions, const WriteHandlerType& handler) = 0;
 
         virtual LinkAddressType Update(const LinkType& restrictions, const LinkType& substitution, const WriteHandlerType& handler) = 0;
 
-        virtual LinkAddressType Update(LinkType&& restrictions, LinkType&& substitution, const WriteHandlerType& handler) = 0;
-
         virtual void Delete(const LinkType& restrictions, const WriteHandlerType& handler) = 0;
-
-        virtual void Delete(const LinkType& restrictions, const WriteHandlerType&& handler) = 0;
-
-        virtual void Delete(LinkType&& restrictions, const WriteHandlerType&& handler) = 0;
     };
 }
