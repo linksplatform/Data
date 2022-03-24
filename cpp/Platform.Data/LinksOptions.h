849bd75d633694f87b565e2cb84e37a3935c68b7
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-24 23:43:07 +0600

    Remove any usings of LinkType

--- cpp/Platform.Data/LinksOptions.h
+++ cpp/Platform.Data/LinksOptions.h
@@ -3,7 +3,6 @@ namespace Platform::Data
     template<typename TLinkAddress = std::uint64_t, typename TWriteHandler = std::function<TLinkAddress(std::vector<TLinkAddress>, std::vector<TLinkAddress>)>, typename TReadHandler = std::function<TLinkAddress(std::vector<TLinkAddress>)>, LinksConstants<TLinkAddress> VConstants = LinksConstants<TLinkAddress>{true}>
     struct LinksOptions
     {
-        using LinkType = std::vector<TLinkAddress>;
         using LinkAddressType = TLinkAddress;
         using WriteHandlerType = TWriteHandler;
         using ReadHandlerType = TReadHandler;
