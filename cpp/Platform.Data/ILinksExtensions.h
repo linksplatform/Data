3e8c8367e66ba3f8cc4e930862a5b75f16a136aa
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-24 21:17:36 +0600

    Check length before casting pack to LinkType

--- cpp/Platform.Data/ILinksExtensions.h
+++ cpp/Platform.Data/ILinksExtensions.h
@@ -17,7 +17,16 @@
     template<typename TStorage>
     static typename TStorage::LinkAddressType Create(TStorage& storage, std::convertible_to<typename TStorage::LinkAddressType> auto ...substitutionPack)
     {
-        typename TStorage::LinkType substitution { static_cast<typename TStorage::LinkAddressType>(substitutionPack)... };
+        constexpr auto length = sizeof...(substitutionPack);
+        typename TStorage::LinkType substitution;
+        if constexpr (0 == length)
+        {
+            substitution = { storage.Constants.Any, storage.Constants.Any, storage.Constants.Any };
+        }
+        else
+        {
+            substitution = { static_cast<typename TStorage::HandlerParameterType>(substitutionPack)... };
+        }
         auto $continue { storage.Constants.Continue };
         typename TStorage::LinkAddressType createdLinkAddress;
         storage.Create(substitution, [&createdLinkAddress, $continue] (const typename TStorage::LinkType& before, const typename TStorage::LinkType& after)
